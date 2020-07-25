1267117a0662b71432c8a0ee9fff9fe1a4319155
JCoMcL <rapcyss@gmail>
2020-07-25 12:45:05 +0100

    1.1 partial work on updating isDeviceSuitable

--- queue.cpp
+++ queue.cpp
@@ -5,7 +5,9 @@
 
 struct QueueFamilyIndices {
 	std::optional<uint32_t> graphicsFamily;
-	bool isComplete() { return graphicsFamily.has_value(); }
+	std::optional<uint32_t> presentFamily;
+
+	bool isComplete() { return graphicsFamily.has_value() && presentFamily.has_value(); }
 };
 
 //TODO: compile/find a list of queue families
