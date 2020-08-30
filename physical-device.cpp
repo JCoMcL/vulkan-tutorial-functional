900c553442d4cafe429b14244b5f274e3c60490f
JCoMcL <rapcyss@gmail>
2020-08-30 19:35:32 +0100

    add checks for enabled extensions - swapchain, in particular

--- physical-device.cpp
+++ physical-device.cpp
@@ -1,8 +1,10 @@
 #include <vulkan/vulkan.h>
 #include <iostream>
+#include <set>
 #include "queue.cpp"
 
 bool isDeviceSuitable(PhysicalDeviceQueueFamilyIndices device);
+bool extensionsSupported(VkPhysicalDevice pDev, std::vector<const char*> extensions);
 
 PhysicalDeviceQueueFamilyIndices createPhysDev(VkInstance instance, VkSurfaceKHR surface) {
 	uint32_t deviceCount = 0;
@@ -29,13 +31,24 @@ PhysicalDeviceQueueFamilyIndices createPhysDev(VkInstance instance, VkSurfaceKHR
 
 //TODO this function seems like may need to be contantly expanded based on the specifics of the program. Ideally there would be only one such function and it would be referenced directly by main
 bool isDeviceSuitable(PhysicalDeviceQueueFamilyIndices devInds) {
-	/*
-	VkPhysicalDeviceProperties deviceProperties;
-	vkGetPhysicalDeviceProperties(device, &deviceProperties);
+	const std::vector<const char*> extensions = { VK_KHR_SWAPCHAIN_EXTENSION_NAME }; //TODO: clearly this doesn't belong here
+	return (
+		devInds.inds.isComplete() &&
+		extensionsSupported(devInds.dev, extensions)
+	);
+} 
 
-	VkPhysicalDeviceFeatures deviceFeatures;
-	vkGetPhysicalDeviceFeatures(device, &deviceFeatures);
-	*/
-	return devInds.inds.isComplete();
+bool extensionsSupported(VkPhysicalDevice pDev, std::vector<const char*> extensions) {
+	uint32_t extensionCount;
+	vkEnumerateDeviceExtensionProperties(pDev, nullptr, &extensionCount, nullptr);
+
+	std::vector<VkExtensionProperties> availableExtensions(extensionCount);
+	vkEnumerateDeviceExtensionProperties(pDev, nullptr, &extensionCount, availableExtensions.data());
+
+	std::set<std::string> requiredExtensions(extensions.begin(), extensions.end());
+
+	for (const auto& extension : availableExtensions)
+		requiredExtensions.erase(extension.extensionName);
+
+	return requiredExtensions.empty();
 }
-
